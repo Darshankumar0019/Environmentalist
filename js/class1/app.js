60955681448186f18d4e0804f87955e105f3ef65
Darshankumar0019 <darshankumar0019@gmail.com>
2023-04-07 08:00:55 +0500

    hi

---
+++ js/class1/app.js
@@ -0,0 +1,31 @@
+var fullname="darshan"
+var fathername="popat"
+var email="d#############19@gmail.com"
+var mobilenumber="0332####412"
+var cnicnumber="44303######61"
+var courses="web development"
+var qualification="graduation"
+var gender="male"
+console.log("Full Name:-" +fullname)
+console.log("Father Name:- "+fathername)
+console.log("Email:- "+email)
+console.log("Mobile Number:-" +mobilenumber)
+console.log("CNIC Number:-" +cnicnumber)
+console.log("Courses:- "+courses)
+console.log("Qualification:- "+qualification)
+console.log("Gender:- "+gender)
+document.write("Full Name:-" +fullname)
+document.write("<br>")
+document.write("Father Name:- "+fathername)
+document.write("<br>")
+document.write("Email:- "+email)
+document.write("<br>")
+document.write("Mobile Number:-" +mobilenumber)
+document.write("<br>")
+document.write("CNIC Number:-" +cnicnumber)
+document.write("<br>")
+document.write("Courses:- "+courses)
+document.write("<br>")
+document.write("Qualification:- "+qualification)
+document.write("<br>")
+document.write("Gender:- "+gender)
